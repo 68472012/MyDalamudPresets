318399eaa9169fee67f95fd729e76dd1f51f7a00
86472012 <mozartlll_nachtlied@yahoo.co.jp>
2025-09-21 23:28:58 +0900

    フォルダ変更

---
+++ SomethingNeedDoing/Leves/Tradecraft/6. Endwalker/キャロットラペ/キャロットラペ NativeLoop.lua
@@ -0,0 +1,15 @@
+--[=====[
+[[SND Metadata]]
+author: 小鳥遊レイ
+version: 1.0
+description: |
+  オールド・シャーレアンのリーヴ受注NPC「グリッグ」と納品NPC「アルダイルン」の間に立って実行すると自動でリーヴ受注～納品を行います。
+  Nativeマクロです。
+
+plugin_dependencies:
+普通に改行でok
+
+configs:
+普通に改行でok
+[[End Metadata]]
+--]=====]
